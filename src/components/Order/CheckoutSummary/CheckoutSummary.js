eeacdba14de1f3868da734625e815da3ad6f6f1d
David Reyes-Pucheta <davidpucheta@gmail.com>
2019-11-27 14:02:37 -0600

    Navigating to the Contact-Data component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -11,12 +11,12 @@ const checkoutSummary = (props) => {
                 <Burger ingredients={props.ingredients} />
             </div>
             <Button
-                clicked
+                clicked={props.checkoutCancelled}
                 btnType="Danger">
                 CANCEL
             </Button>
             <Button
-                clicked
+                clicked={props.checkoutContinued}
                 btnType="Success">
                 CONTINUE
             </Button>
