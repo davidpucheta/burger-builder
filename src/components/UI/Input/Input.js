ddf8ed702a8b3b6c0e39edb25b174b68a4d3f683
David Reyes-Pucheta <davidpucheta@gmail.com>
2019-12-19 17:30:55 -0600

    Adding dropdown component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,25 +4,39 @@ import classes from './Input.module.css';
 const input = (props) => {
     let inputElement = null;
 
-    switch (props.inputtype) {
-        case 'input':
-            inputElement = <input 
-                className={classes.InputElement} 
-                {...props.elementConfig} 
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
                 value={props.value}
-                />;
-            break;
-        case 'textarea':
-            inputElement = <textarea 
-            className={classes.InputElement} 
-            {...props.elementConfig} 
-            value={props.value}
             />;
             break;
+        case ('textarea'):
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+            />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}>
+                        {props.elementConfig.options.map( option =>(
+                                <option key={option.value} 
+                                    value={option.value}>
+                                   {option.displayValue}
+                                </option>
+                            ))}
+                </select>
+            );
+            break;
         default:
-            inputElement = <input 
-            className={classes.InputElement} 
-                {...props.elementConfig} 
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
                 value={props.value}
             />;
             break;
